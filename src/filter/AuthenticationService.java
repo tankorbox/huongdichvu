000b4e65e1571245a49affb9dda5b12cf44b17da
Hoang Linh Tan <tankorbox@gmail.com>
2017-09-30 21:37:14 +0700

    add filters

---
+++ src/filter/AuthenticationService.java
@@ -0,0 +1,36 @@
+package filter;
+
+
+import java.io.IOException;
+import java.util.Base64;
+import java.util.StringTokenizer;
+
+public class AuthenticationService {
+	public boolean authenticate(String authCredentials) {
+
+		if (null == authCredentials)
+			return false;
+		// header value format will be "Basic encodedstring" for Basic
+		// authentication. Example "Basic YWRtaW46YWRtaW4="
+		final String encodedUserPassword = authCredentials.replaceFirst("Basic"
+				+ " ", "");
+		String usernameAndPassword = null;
+		try {
+			byte[] decodedBytes = Base64.getDecoder().decode(
+					encodedUserPassword);
+			usernameAndPassword = new String(decodedBytes, "UTF-8");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		final StringTokenizer tokenizer = new StringTokenizer(
+				usernameAndPassword, ":");
+		final String username = tokenizer.nextToken();
+		final String password = tokenizer.nextToken();
+
+		// we have fixed the userid and password as admin
+		// call some UserService/LDAP here
+		boolean authenticationStatus = "admin".equals(username)
+				&& "admin".equals(password);
+		return authenticationStatus;
+	}
+}
